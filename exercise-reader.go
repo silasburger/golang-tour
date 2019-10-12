e74e3bcefb6828bc6def3aec10d2dd62ec2a4270
Silas Burger <silas.burger@ooma.com>
2019-10-12 15:44:34 -0700

    added reader example

---
+++ exercise-reader.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"golang.org/x/tour/reader"
+)
+
+type MyReader struct{}
+
+func (m MyReader) Read(b []byte) (int, error) {
+	b = b[:cap(b)]
+	for i := range b {
+		b[i] = 'A'
+	}
+	return len(b), nil
+}
+
+func main() {
+	reader.Validate(MyReader{})
+}
