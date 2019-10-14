774bcce4bf929b02d082c3c500201a55b208ef70
Silas Burger <silas.burger@ooma.com>
2019-10-14 11:13:27 -0700

    finish rot reader

--- exercise-reader.go
+++ exercise-reader.go
@@ -1,19 +1,46 @@
 package main
 
 import (
-	"golang.org/x/tour/reader"
+	"io"
+	"os"
+	"strings"
 )
 
-type MyReader struct{}
+type rot13Reader struct {
+	r io.Reader
+}
 
-func (m MyReader) Read(b []byte) (int, error) {
-	b = b[:cap(b)]
-	for i := range b {
-		b[i] = 'A'
+func rot13(x byte) byte {
+	if x >= 'A' && x <= 'M' {
+		return x + 13
 	}
-	return len(b), nil
+
+	if x >= 'N' && x <= 'Z' {
+		return x - 13
+	}
+
+	if x >= 'a' && x <= 'm' {
+		return x + 13
+	}
+
+	if x >= 'n' && x <= 'z' {
+		return x - 13
+	}
+
+	return x
+
+}
+
+func (r13 rot13Reader) Read(b []byte) (int, error) {
+	n, err := r13.r.Read(b)
+	for i := range b {
+		b[i] = rot13(b[i])
+	}
+	return n, err
 }
 
 func main() {
-	reader.Validate(MyReader{})
+	s := strings.NewReader("Lbh penpxrq gur pbqr!")
+	r := rot13Reader{s}
+	io.Copy(os.Stdout, &r)
 }
